4eca15973629b356d64d83fdeea6b55b09c380d7
gulernurdonmez <gule0153@stud.kea.dk>
2022-04-25 20:11:38 +0200

    create employee works

--- src/main/java/com/example/demo/controllers/EmployeeController.java
+++ src/main/java/com/example/demo/controllers/EmployeeController.java
@@ -6,9 +6,11 @@ import com.example.demo.repositories.IRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.WebRequest;
 
-import java.util.List;
-//testetststt
+import java.sql.Date;
+
 @Controller
 public class EmployeeController {
 
@@ -19,6 +21,32 @@ public class EmployeeController {
     public String allEmployees(Model model){
         model.addAttribute("employees", employeeIRepository.getAllEntities());
         return "employees";
+    }
+
+    @GetMapping("/createEmployee")
+    public String wishList(){
+        return "createEmployee";
+    }
+
+    @PostMapping("/createEmployee")
+    public String wishListCreation(WebRequest dataFromForm){
+
+        int id = -1;
+
+        String name = dataFromForm.getParameter("name");
+        String job = dataFromForm.getParameter("job");
+        int commission = Integer.parseInt(dataFromForm.getParameter("commission"));
+        int departmentNr = Integer.parseInt(dataFromForm.getParameter("departmentNr"));
+        int manager = Integer.parseInt(dataFromForm.getParameter("manager"));
+        int salary = Integer.parseInt(dataFromForm.getParameter("salary"));
+        Date date = Date.valueOf(dataFromForm.getParameter("date"));
+
+
+        Employee employee = new Employee(id, name, job, manager, date, salary, commission, departmentNr);
+
+        employeeIRepository.create(employee);
+
+        return "redirect:/createEmployee";
     }
 
 }
--- src/main/java/com/example/demo/repositories/EmployeeRepository.java
+++ src/main/java/com/example/demo/repositories/EmployeeRepository.java
@@ -4,10 +4,8 @@ import com.example.demo.models.Department;
 import com.example.demo.models.Employee;
 import com.example.demo.utility.DatabaseConnectionManager;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -45,11 +43,31 @@ public class EmployeeRepository implements IRepository<Employee>{
 
     @Override
     public Employee getSingleById(int id) {
+        Connection conn = DatabaseConnectionManager.getConnection();
         return null;
     }
 
     @Override
     public boolean create(Employee entity) {
-        return false;
+        Connection conn = DatabaseConnectionManager.getConnection();
+        try {
+            PreparedStatement pstmt = conn.prepareStatement("INSERT INTO employees(`employee_name`, `job`, `manager`, " +
+                    "`hiredate`, `salary`, `commission`, `department_number`)" + " VALUES (?,?,?,?,?,?,?)");
+
+            pstmt.setString(1, entity.getEmployeeName().toUpperCase());
+            pstmt.setString(2, entity.getJob().toUpperCase());
+            pstmt.setInt(3, entity.getManager());
+            pstmt.setDate(4, Date.valueOf(LocalDate.now()));
+            pstmt.setInt(5, entity.getSalary());
+            pstmt.setInt(6, entity.getCommission());
+            pstmt.setInt(7, entity.getDepartmentNumber());
+
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println("something is wrong");
+            e.printStackTrace();
+        }
+        return true;
     }
 }
